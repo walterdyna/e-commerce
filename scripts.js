586ddab1dd4ac4e2ae719ac6d0c5c2d25c1385b3
walterdyna <alexdyna@gmail.com>
2024-04-03 20:37:02 -0300

    Caranguejo

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function showDetails(id) {
+    document.getElementById(`detalhes-${id}`).style.display = "block";
+}
+
+function hideDetails(id) {
+    document.getElementById(`detalhes-${id}`).style.display = "none";
+}
